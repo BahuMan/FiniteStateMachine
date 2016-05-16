64c77f599901aada756a4e9ca080ebe23f40d3c6
Bart Huylebroeck <BahuMan@users.noreply.github.com>
2016-05-16 10:01:59 +0200

    experiment in monobehaviour + improved gitignore

--- Assets/cryingpants/FSMBehaviour.cs
+++ Assets/cryingpants/FSMBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections.Generic;
 
 namespace cryingpants
@@ -6,13 +7,62 @@ namespace cryingpants
 
     public class FSMBehaviour: MonoBehaviour
     {
-        public List<Vector3> states;
+
+        public class FiniteState
+        {
+            public string name { get { return name; } internal set { name = value; } }
+            public FiniteState(string s)
+            {
+                name = s;
+                StateListeners = new List<FiniteStateHandler<string>>();
+            }
+
+            //list of all objects that will be called when this state (1) becomes current, (2) is current, or (3) stops being current state
+            internal List<FiniteStateHandler<string>> StateListeners;
+
+            //all the possible new states accessible from this current state:
+            internal HashSet<FiniteState> Transitions;
+        }
+
+        private FiniteState currentState;
 
         public void Start()
         {
-            Debug.Log("Initing list");
-            states.Add(new Vector3(0f, 0f, 0f));
+            Debug.Log("no initing done (yet)");
         }
 
+        public void update()
+        {
+            string next = currentState.name;
+            foreach (FiniteStateHandler<string> l in currentState.StateListeners)
+            {
+                string returned = l.OnStateUpdate(currentState.name);
+                //status change requested?
+                if (!returned.Equals(currentState.name))
+                {
+                    //if somebody else already requested a status change, we have a conflict
+                    if (!next.Equals(currentState.name) && !next.Equals(returned))
+                    {
+                        throw new RequestedTransitionConflictException<string>("FSM: 2 different transitions requested in one update", currentState.name, next, returned);
+                    }
+                    next = returned;
+                }
+            }
+            handleTransition(next);
+        }
+
+        private void handleTransition(string newState)
+        {
+            if (newState.Equals(currentState.name)) return;
+            foreach (FiniteStateHandler<string> l in currentState.StateListeners)
+            {
+                l.OnStateExit(currentState.name);
+            }
+            currentState = allStates[newState];
+            foreach (FiniteStateHandler<string> l in currentState.StateListeners)
+            {
+                l.OnStateEnter(currentState.name);
+            }
+        }
     }
 }
